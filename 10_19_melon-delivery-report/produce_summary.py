b117d72cf194c8e751d4073941195f04a4149624
nanersmariee <anna.marie19@gmail.com>
2019-10-24 02:48:44 +0000

    called function for each separate day

--- 10_19_melon-delivery-report/produce_summary.py
+++ 10_19_melon-delivery-report/produce_summary.py
@@ -1,6 +1,6 @@
 
 def get_delivery_report (file):
-    print("Day 1")
+    
     the_file = open(file)
     for line in the_file:
         line = line.rstrip()
@@ -16,34 +16,43 @@ def get_delivery_report (file):
         count, melon, amount))
     #the_file.close()
 
+print("Day 1")
 get_delivery_report("um-deliveries-20140519.txt")
-
+print()
 
 print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
+get_delivery_report("um-deliveries-20140520.txt")
+print()
 
 print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+get_delivery_report("um-deliveries-20140521.txt")
+print()
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
